0c2ea37da235225802599c9289ae9a8746add46c
Scala Steward <scala_steward@virtuslab.com>
2022-11-19 03:52:27 +0000

    Update jcl-over-slf4j to 2.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     "com.typesafe" % "config" % "1.4.2",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5",
     "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.19.0",
-    "org.slf4j" % "jcl-over-slf4j" % "2.0.3",
+    "org.slf4j" % "jcl-over-slf4j" % "2.0.4",
     "com.typesafe.akka" %% "akka-actor" % "2.6.20",
     "com.typesafe.akka" %% "akka-stream" % "2.6.20",
     "com.typesafe.akka" %% "akka-http" % "10.2.10",
